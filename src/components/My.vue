f8ee93a820f1b341bf52d1c5f7143e97c8820500
hanfenglin <han_fenglin@163.com>
2019-08-30 16:44:43 +0800

    个人中心页面

--- src/components/My.vue
+++ src/components/My.vue
@@ -4,14 +4,14 @@
       <h1 class="weui-header-title">个人中心</h1>
     </div>
     <div class="content">
-      <div class="weui-media-box weui-media-box_appmsg">
+      <div class="userinfo">
         <div class="weui-avatar-circle">
           <img src="../../static/img/home/touxiang.png">
           <span class="weui-icon-success weui-icon-safe-warn"></span>
         </div>
-        <div class="userinfo">
-          <p >韩凤麟</p>
-          <p class="">智能业务中心</p>
+        <div class="user_name">
+         <p class="name">韩凤麟</p>
+          <p class="dept">智能业务中心</p>
         </div>
       </div>
 
@@ -32,10 +32,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.content{
-  margin: 2rem 2rem 0 2rem;
-}
-.userinfo{
-
-}
+  .content{
+     margin: 2rem 2rem 0 2rem;
+   }
+  .userinfo{
+    display:flex;
+    justify-content:flex-start;
+    align-items: center;
+  }
+  .name{
+    font-size: 16px;
+  }
+  .dept{
+    font-size: 12px;
+    color: gray;
+  }
+  .user_name p {
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    justify-content: flex-start;
+    margin: 0 10px;
+  }
 </style>
